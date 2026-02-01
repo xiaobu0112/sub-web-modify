5576473c85969b14688010d090a9b43df1e2cdd7
xiaobu0112 <113268846+xiaobu0112@users.noreply.github.com>
2026-02-01 19:15:36 +0800

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,10 +338,14 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
+          "自用专属后端": "https://subapi.xiaobu.de5.net",// 为您的后端取名
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://api.v1.mk",
+          "nameless13提供": "https://www.nameless13.com",
+          "subconverter作者提供": "https://sub.xeton.dev",
+          "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
           { value: "https://subapi.cmliussss.net" },
@@ -1304,3 +1308,4 @@ export default {
   }
 };
 </script>
+
